cc929ddc7191e3160a6dde999d54ba376f0472da
Edoardo Giambuzzi <edoardo.giambuzzi@gmail.com>
2024-02-23 17:04:48 +0100

    fix major css issues

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,48 +1,37 @@
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
+import "../styles/about.css";
 
 export const About = () => {
   return (
     <>
       <Header />
-      <div className="about-row-container">
-        <h3>St. Alban & the dragon</h3>
-
-        <img
-          className="paragraph-break"
-          src="https://res.cloudinary.com/dgwvbd9ki/image/upload/v1706785230/spellworth-butterbeer/paragraph-break_wvbzod.png"
-          alt="spellworth-logo"
-        />
-
+      <div className="about-page-container">
         <div className="about-row-1">
           <img
-            className="about-img-1"
-            src="https://res.cloudinary.com/dgwvbd9ki/image/upload/v1706864581/spellworth-butterbeer/djambo1990_51954_A_hyperrealistic_black_and_white_photograph_of_b041ef89-5a18-43fb-b17b-5b17054527a8_vbqeno.png"
+            src="https://res.cloudinary.com/dgwvbd9ki/image/upload/v1706864580/spellworth-butterbeer/djambo1990_51954_A_hyperrealistic_black_and_white_photograph_of_fcfc6daf-d3b1-44b9-bcf1-cfc42db94a6d_zvxxr3.png"
             alt=""
           />
-          <p className="about-paragraph-1">
-            For centuries, the shadow of the dragon Zefirakiel loomed large over
-            the lands of St. Alban, casting a dark and oppressive cloud over its
-            inhabitants. Tales whispered of its fierce reign, its fiery breath
-            scorching fields and terrorizing villages. Many brave wizards and
-            witches attempted to rid the land of this ancient menace, but all
-            efforts proved futile against the dragon's formidable power.
-          </p>
-
+          <div className="about-text-container">
+            <p>
+              For centuries, the shadow of the dragon Zefirakiel loomed large
+              over the lands of St. Alban, casting a dark and oppressive cloud
+              over its inhabitants. Tales whispered of its fierce reign, its
+              fiery breath scorching fields and terrorizing villages. Many brave
+              wizards and witches attempted to rid the land of this ancient
+              menace, but all efforts proved futile against the dragon's
+              formidable power.
+            </p>
+          </div>
+        </div>
+        <div className="about-row-2">
           <img
-            className="paragraph-break"
-            src="https://res.cloudinary.com/dgwvbd9ki/image/upload/v1706785230/spellworth-butterbeer/paragraph-break_wvbzod.png"
-            alt="spellworth-logo"
+            src="https://res.cloudinary.com/dgwvbd9ki/image/upload/v1706864582/spellworth-butterbeer/djambo1990_51954_A_hyperrealistic_black_and_white_photograph_of_8c0c1ac3-cb5d-4c18-b42e-013127b3d900_u3rnf3.png"
+            alt=""
           />
-
-          <div className="about-row-2">
-            <img
-              className="about-img-2"
-              src="https://res.cloudinary.com/dgwvbd9ki/image/upload/v1706864580/spellworth-butterbeer/djambo1990_51954_A_hyperrealistic_black_and_white_photograph_of_fcfc6daf-d3b1-44b9-bcf1-cfc42db94a6d_zvxxr3.png"
-              alt=""
-            />
-
-            <p className="about-paragraph-2">
+          <div className="about-text-container">
+            <p>
+              {" "}
               It wasn't until the emergence of our esteemed ancestor, Antonius
               Grenwyck Spellworth, that hope began to dawn on the beleaguered
               land. Through cunning strategy and unwavering determination,
@@ -53,20 +42,14 @@ export const About = () => {
               tyranny.
             </p>
           </div>
-
+        </div>
+        <div className="about-row-3">
           <img
-            className="paragraph-break"
-            src="https://res.cloudinary.com/dgwvbd9ki/image/upload/v1706785230/spellworth-butterbeer/paragraph-break_wvbzod.png"
-            alt="spellworth-logo"
+            src="https://res.cloudinary.com/dgwvbd9ki/image/upload/v1706864577/spellworth-butterbeer/djambo1990_51954_A_hyperrealistic_black_and_white_photograph_of_3d0ccd2f-004a-4128-935d-98c7012262aa_ii4qbd.png"
+            alt=""
           />
-
-          <div className="about-row-3">
-            <img
-              className="about-img-3"
-              src="https://res.cloudinary.com/dgwvbd9ki/image/upload/v1706864178/spellworth-butterbeer/djambo1990_51954_a_huge_structure_from_which_the_body_of_a_humo_2c8c9045-1b72-46ac-91c1-670e665cc19c_p85gyx.png"
-              alt=""
-            />
-            <p className="about-paragraph-3">
+          <div className="about-text-container">
+            <p>
               In recognition of his heroic deed, the Ministry of Magic bestowed
               upon Antonius the title to the liberated lands of St. Alban. And
               it was upon this sacred soil, enriched by the blood of the
@@ -76,21 +59,15 @@ export const About = () => {
               possibility.
             </p>
           </div>
-
+        </div>
+        <div className="about-row-4">
           <img
-            className="paragraph-break"
-            src="https://res.cloudinary.com/dgwvbd9ki/image/upload/v1706785230/spellworth-butterbeer/paragraph-break_wvbzod.png"
-            alt="spellworth-logo"
+            src="https://res.cloudinary.com/dgwvbd9ki/image/upload/v1706864178/spellworth-butterbeer/djambo1990_51954_a_huge_structure_from_which_the_body_of_a_humo_57ad4ad9-c5d6-46d7-9e09-b1c40888c6e2_fuhdsi.png"
+            alt=""
           />
-
-          <div className="about-row-4">
-            <img
-              className="about-img-4"
-              src="https://res.cloudinary.com/dgwvbd9ki/image/upload/v1706864178/spellworth-butterbeer/djambo1990_51954_a_huge_structure_from_which_the_body_of_a_humo_57ad4ad9-c5d6-46d7-9e09-b1c40888c6e2_fuhdsi.png"
-              alt=""
-            />
-
-            <p className="about-paragraph-4">
+          <div className="about-text-container">
+            <p>
+              {" "}
               Thus, from the ashes of adversity arose a new era of prosperity
               for the Spellworth lineage. The fertile land bore witness to the
               birth of a beverage unlike any other – Butterbeer – whose
@@ -98,9 +75,9 @@ export const About = () => {
               with the name Spellworth, echoing through generations to come.
             </p>
           </div>
-          <Footer />
         </div>
       </div>
+      <Footer />
     </>
   );
 };
